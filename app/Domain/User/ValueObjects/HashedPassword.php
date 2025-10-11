0e5b45be300856fb19a3c91026246e2fc0270fc2
Nuwanthi <mail.nuwanthi@gmail.com>
2025-10-11 06:10:32 +0530

    feat: add guards to verify if passed string is a hashed value and to remove leading/trailing spaces

--- app/Domain/User/ValueObjects/HashedPassword.php
+++ app/Domain/User/ValueObjects/HashedPassword.php
@@ -3,16 +3,28 @@
 namespace App\Domain\User\ValueObjects;
 
 use Illuminate\Support\Facades\Hash;
+use InvalidArgumentException;
+
 
 class HashedPassword
 {
 
-    public function __construct(private string $hashed) {}
+    private string $hashed;
+    public function __construct(string $hashedString)
+    {
+        if (strlen($hashedString) === 0) {
+            throw new InvalidArgumentException("Hashed password cannot be empty.");
+        }
+        if (!Hash::isHashed($hashedString)) {
+            throw new InvalidArgumentException("Invalid hash value.");
+        }
+        $this->hashed = $hashedString;
+    }
 
     public static function fromPlainText(string $plainText): self
     {
-        if (strlen($plainText) < 8) {
-            throw new \InvalidArgumentException("Password must be at 8 characters.");
+        if (strlen(trim($plainText)) < 8) {
+            throw new InvalidArgumentException("Password must be at 8 characters.");
         }
         return new self(Hash::make($plainText));
     }
